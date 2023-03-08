8339dc740266e7e5aed0309141388a5294e89ad0
VaiTon <eyadlorenzo@gmail.com>
2023-03-08 23:24:34 +0100

    fix: use katex instead of the extension to enable customizations

--- src/routes/[...dir]/[file=markdown]/+page.svelte
+++ src/routes/[...dir]/[file=markdown]/+page.svelte
@@ -2,16 +2,23 @@
 	import { onMount } from 'svelte';
 	import type { PageData } from './$types';
 	import { marked } from 'marked';
-	import markedKatex from '$lib/marked-katex';
-	marked.use(markedKatex());
+	// @ts-expect-error - auto-render is not in the type definitions
+	import autoRender from 'katex/dist/contrib/auto-render.mjs';
 
 	export let data: PageData;
 
 	let markdown: HTMLElement;
 
 	onMount(async () => {
-		const html = await marked(data.body, { async: true });
-		markdown.innerHTML = html;
+		markdown.innerHTML = await marked(data.body, { async: true });
+		autoRender(markdown, {
+			delimiters: [
+				{ left: '$$', right: '$$', display: true },
+				{ left: '\\[', right: '\\]', display: true },
+				{ left: '$', right: '$', display: false },
+				{ left: '\\(', right: '\\)', display: false }
+			]
+		});
 	});
 </script>
 
