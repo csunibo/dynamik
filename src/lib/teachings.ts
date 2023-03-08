efc36d6cdbc421ed0ff899d9a5f6f05d579828b4
VaiTon <eyadlorenzo@gmail.com>
2023-03-08 23:23:49 +0100

    feat: added more teachings to ingegneria

--- src/lib/teachings.ts
+++ src/lib/teachings.ts
@@ -40,11 +40,30 @@ const TEACHINGS: Course[] = [
 			{
 				year: 1,
 				teachings: [
+					{ name: 'Fondamenti di informatica 1', url: 'ing-fondamenti-di-informatica-1-t' },
 					{ name: 'Analisi 1', url: 'ing-analisi-1-t' },
 					{ name: 'Algebra e geometria', url: 'ing-algebra-e-geometria-t' },
-					{ name: 'Fondamenti di informatica 1', url: 'ing-fondamenti-di-informatica-1-t' },
+
+					{ name: 'Fondamenti di informatica 2', url: 'ing-fondamenti-di-informatica-2-t' },
 					{ name: 'Analisi 2', url: 'ing-analisi-2-t' },
-					{ name: 'Fondamenti di informatica 2', url: 'ing-fondamenti-di-informatica-2-t' }
+					{ name: 'Reti logiche', url: 'ing-reti-logiche-t' }
+				]
+			},
+			{
+				year: 2,
+				teachings: [
+					{ name: 'Matematica applicata', url: 'ing-matematica-applicata-t' },
+					{ name: 'Calcolatori elettronici', url: 'ing-calcolatori-elettronici-t' },
+					{ name: 'Fisica generale', url: 'ing-fisica-generale-t' },
+					{ name: 'Sistemi informativi', url: 'ing-sistemi-informativi-t' },
+
+					{ name: 'Sistemi operativi', url: 'ing-sistemi-operativi-t' },
+					{ name: 'Fondamenti di telecomunicazioni', url: 'ing-fondamenti-di-telecomunicazioni-t' },
+					{
+						name: 'Economia e organizzazione aziendale',
+						url: 'ing-economia-e-organizzazione-aziendale-t'
+					},
+					{ name: 'Elettrotecnica', url: 'ing-elettrotecnica-t' }
 				]
 			}
 		]
