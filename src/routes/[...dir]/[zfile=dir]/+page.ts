bfe8ed7054655769199b79a4d754bbf575cc02d7
VaiTon <eyadlorenzo@gmail.com>
2023-03-08 23:23:28 +0100

    chore: better error message if load fails

--- src/routes/[...dir]/[zfile=dir]/+page.ts
+++ src/routes/[...dir]/[zfile=dir]/+page.ts
@@ -3,12 +3,14 @@ import type { Statik } from '$lib/api';
 import { STATIK_URL } from '$lib/const';
 
 export const load = (async ({ fetch, params }) => {
-  const res = await fetch(STATIK_URL(params.dir ? params.dir + '/' + params.zfile : params.zfile));
-  if (!res.ok) {
-    throw new Error(`Failed to load manifest at ${STATIK_URL(params.dir)}`);
-  }
-  const manifest: Statik = await res.json();
-  return {
-    manifest
-  };
+	const path = params.dir ? params.dir + '/' + params.zfile : params.zfile;
+	const statikUrl = STATIK_URL(path);
+	const res = await fetch(statikUrl);
+	if (!res.ok) {
+		throw new Error(`Failed to load manifest at ${statikUrl}`);
+	}
+	const manifest: Statik = await res.json();
+	return {
+		manifest
+	};
 }) satisfies PageLoad;
