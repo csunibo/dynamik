9e665ef49739c694098bf23b83e6f2eda0b234e6
VaiTon <eyadlorenzo@gmail.com>
2023-03-09 22:47:24 +0100

    fix: static deploy for github pages

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,7 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const BASE_PATH = process.env.BASE_PATH || '';
-const VERCEL = process.env.VERCEL;
+const GITHUB_PAGES = process.env.GITHUB_ACTIONS === 'true';
 
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -16,12 +16,11 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: VERCEL
-			? vercelAdapter()
-			:staticAdapter({
+		adapter: GITHUB_PAGES
+			? staticAdapter({
 				fallback: '404.html'
 			})
-			,
+			: vercelAdapter(),
 		paths: {
 			base: BASE_PATH
 		}
