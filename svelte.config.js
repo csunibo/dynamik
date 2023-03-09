7a4049c5eb37de1ae3b4ff8a9b52dca26118cb09
VaiTon <eyadlorenzo@gmail.com>
2023-03-09 19:29:09 +0100

    chore: enable vercel adapter
    Use adapter-vercel if we're using vercel.

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,26 @@
-import adapter from '@sveltejs/adapter-static';
+import staticAdapter from '@sveltejs/adapter-static';
+import vercelAdapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const BASE_PATH = process.env.BASE_PATH || '';
+const VERCEL = process.env.VERCEL;
+
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
+
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			fallback: '404.html'
-		}),
+		adapter: VERCEL
+			? staticAdapter({
+				fallback: '404.html'
+			})
+			: vercelAdapter(),
 		paths: {
 			base: BASE_PATH
 		}
