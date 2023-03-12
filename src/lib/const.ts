12635a8db59e193a195c40c68a87fff0993ae09d
VaiTon <eyadlorenzo@gmail.com>
2023-03-12 16:34:46 +0100

    Fix github.dev url

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -2,6 +2,7 @@ const ORG = 'csunibo';
 
 const GH_PAGES_BASE_URL = `https://${ORG}.github.io`;
 const GH_BASE_URL = `https://github.com/${ORG}`;
+const GH_DEV_BASE_URL = `https://github.dev/${ORG}`;
 export const STATIK_URL = (path: string) => `${GH_PAGES_BASE_URL}/${path}/statik.json`;
 export const ASSET_URL = (path: string) => `${GH_PAGES_BASE_URL}/${path}`;
 export const FUZZY_URL = (path: string) => `${GH_PAGES_BASE_URL}/${path}/fuzzy.json`;
@@ -11,6 +12,6 @@ export const EDIT_URLS = (path: string) => {
 	return {
 		github: `${GH_BASE_URL}/${repo}/blob/main/${filePath}`,
 		github_edit: `${GH_BASE_URL}/${repo}/edit/main/${filePath}`,
-		github_dev: `${GH_BASE_URL}/${repo}/blob/main/${filePath}`
+		github_dev: `${GH_DEV_BASE_URL}/${repo}/blob/main/${filePath}`
 	};
 };
