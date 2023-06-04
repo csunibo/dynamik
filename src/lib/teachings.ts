a58ed72ca9dc7b2e8958c1e6fafe40d11e836cb0
VaiTon <eyadlorenzo@gmail.com>
2023-06-04 22:33:17 +0200

    Aggiornati url analisi matematica ing

--- src/lib/teachings.ts
+++ src/lib/teachings.ts
@@ -71,11 +71,11 @@ const TEACHINGS: Course[] = [
 				year: 1,
 				teachings: [
 					{ name: 'Fondamenti di informatica 1', url: 'ing-fondamenti-di-informatica-1-t' },
-					{ name: 'Analisi 1', url: 'ing-analisi-1-t' },
+					{ name: 'Analisi 1', url: 'ing-analisi-matematica-1-t' },
 					{ name: 'Algebra e geometria', url: 'ing-algebra-e-geometria-t' },
 
 					{ name: 'Fondamenti di informatica 2', url: 'ing-fondamenti-di-informatica-2-t' },
-					{ name: 'Analisi 2', url: 'ing-analisi-2-t' },
+					{ name: 'Analisi 2', url: 'ing-analisi-matematica-2-t' },
 					{ name: 'Reti logiche', url: 'ing-reti-logiche-t' }
 				]
 			},
