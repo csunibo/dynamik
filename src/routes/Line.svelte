06e0b0dd657694703ae565b4a253600f4d799354
VaiTon <eyadlorenzo@gmail.com>
2023-10-04 00:20:39 +0200

    style: use border for frontpage buttons chore: extract frontpage buttons into its own component

---
+++ src/routes/Line.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let name: string;
+	export let href: string;
+	export let icon: string;
+</script>
+
+<li>
+	<a class="py-8 justify-center border-base-content border-2 mb-4" {href}> {icon} {name} {icon} </a>
+</li>
