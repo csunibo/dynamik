529c0669462de7fd873eeecef9c078d418aed47b
Alice Benatti <alice17bee@gmail.com>
2023-10-09 14:56:31 +0200

    style(icon): download btn

--- src/lib/components/Line.svelte
+++ src/lib/components/Line.svelte
@@ -46,13 +46,17 @@
 				{data.name}
 			</a>
 		{/if}
-		<span class="whitespace-nowrap text-right text-xs self-center">
+		<span class="whitespace-nowrap flex items-center justify-end text-right text-xs">
 			{#if isFile}
 				{isFile && data.size != '0 B' ? data.size : '-'}
 				{#if data.size != '0 B'}
-					<button class="text-lg ml-3" on:click={downloadFile}>📥</button>
+					<button class="text-lg ml-3" on:click={downloadFile}>
+						<span class="text-accent text-3xl icon-[solar--download-square-bold]"></span>
+					</button>
 				{:else}
-					<button disabled class="text-lg ml-3" style="mix-blend-mode: luminosity">📥</button>
+					<button disabled class="text-lg ml-3" on:click={downloadFile}>
+						<span class="text-neutral text-3xl icon-[solar--download-square-bold]"></span>
+					</button>
 				{/if}
 			{/if}
 		</span>
