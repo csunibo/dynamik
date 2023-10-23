a4b059148079b0ad17f43ffb690c11e3804d2837
Alice Benatti <alice17bee@gmail.com>
2023-10-23 15:31:25 +0200

    pnpm format

--- src/routes/[...dir]/[zfile=dir]/+page.svelte
+++ src/routes/[...dir]/[zfile=dir]/+page.svelte
@@ -172,11 +172,10 @@
 		</div>
 	</div>
 	<div class="flex flex-1 justify-end mr-4 mb-3">
-		<button
-			class="lg:ml-2 p-1 flex items-center rounded-xl bg-primary"
-			on:click={toggleReverse}
-		>
-			<span class="text-base-100 text-xl icon-[solar--sort-vertical-bold-duotone]" class:flip={reverseMode}
+		<button class="lg:ml-2 p-1 flex items-center rounded-xl bg-primary" on:click={toggleReverse}>
+			<span
+				class="text-base-100 text-xl icon-[solar--sort-vertical-bold-duotone]"
+				class:flip={reverseMode}
 			></span>
 		</button>
 	</div>
