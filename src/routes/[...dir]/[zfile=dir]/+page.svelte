ecb17fc886c733bcc5344471b7c0c369e02ac45f
Alice Benatti <74602443+ali-benny@users.noreply.github.com>
2023-11-09 13:35:19 +0100

    fix: breadcrumbs url query (#152)

--- src/routes/[...dir]/[zfile=dir]/+page.svelte
+++ src/routes/[...dir]/[zfile=dir]/+page.svelte
@@ -139,7 +139,7 @@
 						</li>
 					{/if}
 					{#each urlParts as part}
-						{@const href = getPartHref(part)}
+						{@const href = getPartHref(part) + '?' + $page.url.searchParams}
 						<li><a {href}>{part}</a></li>
 					{/each}
 				</ul>
