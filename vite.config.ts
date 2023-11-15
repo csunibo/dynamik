0f1b2ddc8f5374bd1dd233523f91ce1600ccc3f6
Alice Benatti <74602443+ali-benny@users.noreply.github.com>
2023-11-15 15:25:23 +0100

    Add PWA manifest and service-worker for offline caching (#149)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA({
+			/* other pwa options */
+		})
+	],
 	optimizeDeps: {}
 });
