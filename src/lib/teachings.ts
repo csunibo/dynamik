2b1c5fa5fcbd92d5e2311ee656f76278b88c6016
Stefano Volpe <stefano.volpe@student.uva.nl>
2023-11-21 18:11:40 +0100

    feat: wip - teachings.json, degrees.json

--- src/lib/teachings.ts
+++ src/lib/teachings.ts
@@ -6,18 +6,27 @@ export type Teaching = {
 	url: string;
 	chat?: string;
 	optional?: boolean;
+	website: string;
+	professors: string[];
 };
 
-export type TeachingYear = {
+export type YearStudyDiagram = {
+	mandatory?: string[];
+	electives?: string[];
+};
+
+export type Year = {
 	year: number;
-	teachings: Teaching[];
+	chat?: string;
+	teachings: YearStudyDiagram;
 };
 
-export type Course = {
+export type Degree = {
 	id: string;
 	name: string;
 	icon: string;
 	years: TeachingYear[];
+	chat?: string;
 };
 
 export async function isTeachingActive(fetch: typeof window.fetch, teaching: Teaching) {
@@ -29,12 +38,34 @@ export async function isTeachingActive(fetch: typeof window.fetch, teaching: Tea
 	}
 }
 
-export async function getActiveTeachings(fetch: typeof window.fetch, course: Course) {
-	const allTeachings = course.years.flatMap((year) => year.teachings);
+function yearToFlatTeachings(y: Year): Teaching[] {
+	let res: Teaching[] = [];
+	if (y.mandatory) {
+		res += y.mandatory;
+	}
+	if (y.electives) {
+		res += y.electives;
+	}
+	return res;
+}
+
+export async function getActiveTeachings(fetch: typeof window.fetch, course: Degree) {
+	const allTeachings = course.years.flatMap(yearToFlatTeachings);
 	const activeTeachings = await filterAsync(allTeachings, (t) => isTeachingActive(fetch, t));
 
 	return activeTeachings;
 }
 
-import TEACHINGS from '../config/courses.json' assert { type: 'json' };
-export default TEACHINGS as Course[];
+// TEACHINGS
+import teachingsArray from '../config/teachings.json' assert { type: 'json' };
+
+function teachingToIndexedTeaching(t: Teaching): [string, Teaching] {
+	return [t.Url, t];
+}
+
+const TEACHINGS = new Map<string, Teaching>(map(teachingsArray, teachingToIndexedTeaching));
+export { TEACHINGS };
+
+// DEGREES
+import DEGREES from '../config/degrees.json' assert { type: 'Degree[]' };
+export { DEGREES };
