7d829d25ffc3675e9ebc15581c318ba9108e12e5
Luca <luca@teapot.ovh>
2024-02-24 12:32:30 +0100

    fetch the questions on the backend, now that the API is public

--- src/routes/[...dir]/[file=pdf]/+page.ts
+++ src/routes/[...dir]/[file=pdf]/+page.ts
@@ -2,14 +2,39 @@ import sha256 from 'sha256';
 import type { PageLoad } from './$types';
 import { ASSET_URL } from '$lib/const';
 
+interface Question {
+  ID: number;
+  CreatedAt: string;
+  UpdatedAt: string;
+  document: string;
+  start: number;
+  end: number;
+}
+
+interface Document {
+  id: string;
+  questions: Question[];
+  url: string;
+}
+
 export const load = (async ({ fetch, params }) => {
   const path = params.dir + '/' + params.file
   const id = sha256(path)
 
+  const res = await fetch(`http://localhost:3000/documents/${id}`, {
+    credentials: 'include'
+  });
+
+  let questions: Question[] = []
+  const body: Document | { error: string } = await res.json();
+  if (!(body as { error: string }).error) {
+    questions = (body as Document).questions;
+  }
+
   return {
-    questions: [],
     url: ASSET_URL(path),
     id: id,
+    questions,
   };
 }) satisfies PageLoad;
 
