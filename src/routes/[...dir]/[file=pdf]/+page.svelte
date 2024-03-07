18a8af50c85654b49fa7b20495f59d168b1c1e40
faguccio <faga2703@gmail.com>
2024-03-07 17:38:00 +0100

    1. Restyling of Reply Box and logic 2. Close Box after comment is sent

--- src/routes/[...dir]/[file=pdf]/+page.svelte
+++ src/routes/[...dir]/[file=pdf]/+page.svelte
@@ -30,7 +30,20 @@
 	let user = undefined;
 	let edit: boolean = false;
 
+	let replyBoxId: number = null;
+	let replyBoxIndex: number = -1;
+	let parentAnswer: number = null;
+	let parentAnswerAuthor: string;
+
+	$: replyParentString = () => {
+		let res = `Queston ${replyBoxIndex}`;
+		if (parentAnswer != null) {
+			res += `, Answer of ${parentAnswerAuthor}`;
+		}
+	};
+
 	let answers: Answers[] = [];
+	let values: string[] = [];
 
 	let loaded = 0.0; // percentage
 	let pageCanvas: HTMLCanvasElement, fullCanvas: HTMLCanvasElement;
@@ -39,7 +52,6 @@
 		height = 0;
 
 	let pages: PDFPageProxy[] = [];
-	let values: string[] = [];
 	let numPages: number;
 
 	let parentPath = '/';
@@ -160,6 +172,7 @@
 				}
 			});
 			answers[index].addComment(res);
+			replyBoxId = null;
 		} else {
 			toast.push('Error!', {
 				theme: {
@@ -179,6 +192,8 @@
 		}
 		await init();
 	}
+
+	//function showReplyBox(question, index) {}
 </script>
 
 <div class="max-w-6xl p-4 mx-auto">
@@ -202,9 +217,53 @@
 								'http://localhost:3000/login?redirect_uri=http://localhost:5173' +
 								data.url.slice(25))}
 					>
-						Login with <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="32" height="32" viewBox="0,0,256,256">
-							<g fill-opacity="0" fill="#dddddd" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,256v-256h256v256z" id="bgRectangle"></path></g><g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(8.53333,8.53333)"><path d="M15,3c-6.627,0 -12,5.373 -12,12c0,5.623 3.872,10.328 9.092,11.63c-0.056,-0.162 -0.092,-0.35 -0.092,-0.583v-2.051c-0.487,0 -1.303,0 -1.508,0c-0.821,0 -1.551,-0.353 -1.905,-1.009c-0.393,-0.729 -0.461,-1.844 -1.435,-2.526c-0.289,-0.227 -0.069,-0.486 0.264,-0.451c0.615,0.174 1.125,0.596 1.605,1.222c0.478,0.627 0.703,0.769 1.596,0.769c0.433,0 1.081,-0.025 1.691,-0.121c0.328,-0.833 0.895,-1.6 1.588,-1.962c-3.996,-0.411 -5.903,-2.399 -5.903,-5.098c0,-1.162 0.495,-2.286 1.336,-3.233c-0.276,-0.94 -0.623,-2.857 0.106,-3.587c1.798,0 2.885,1.166 3.146,1.481c0.896,-0.307 1.88,-0.481 2.914,-0.481c1.036,0 2.024,0.174 2.922,0.483c0.258,-0.313 1.346,-1.483 3.148,-1.483c0.732,0.731 0.381,2.656 0.102,3.594c0.836,0.945 1.328,2.066 1.328,3.226c0,2.697 -1.904,4.684 -5.894,5.097c1.098,0.573 1.899,2.183 1.899,3.396v2.734c0,0.104 -0.023,0.179 -0.035,0.268c4.676,-1.639 8.035,-6.079 8.035,-11.315c0,-6.627 -5.373,-12 -12,-12z"></path></g></g>
-							</svg>
+						Login with <svg
+							xmlns="http://www.w3.org/2000/svg"
+							x="0px"
+							y="0px"
+							width="32"
+							height="32"
+							viewBox="0,0,256,256"
+						>
+							<g
+								fill-opacity="0"
+								fill="#dddddd"
+								fill-rule="nonzero"
+								stroke="none"
+								stroke-width="1"
+								stroke-linecap="butt"
+								stroke-linejoin="miter"
+								stroke-miterlimit="10"
+								stroke-dasharray=""
+								stroke-dashoffset="0"
+								font-family="none"
+								font-weight="none"
+								font-size="none"
+								text-anchor="none"
+								style="mix-blend-mode: normal"
+								><path d="M0,256v-256h256v256z" id="bgRectangle"></path></g
+							><g
+								fill="#ffffff"
+								fill-rule="nonzero"
+								stroke="none"
+								stroke-width="1"
+								stroke-linecap="butt"
+								stroke-linejoin="miter"
+								stroke-miterlimit="10"
+								stroke-dasharray=""
+								stroke-dashoffset="0"
+								font-family="none"
+								font-weight="none"
+								font-size="none"
+								text-anchor="none"
+								style="mix-blend-mode: normal"
+								><g transform="scale(8.53333,8.53333)"
+									><path
+										d="M15,3c-6.627,0 -12,5.373 -12,12c0,5.623 3.872,10.328 9.092,11.63c-0.056,-0.162 -0.092,-0.35 -0.092,-0.583v-2.051c-0.487,0 -1.303,0 -1.508,0c-0.821,0 -1.551,-0.353 -1.905,-1.009c-0.393,-0.729 -0.461,-1.844 -1.435,-2.526c-0.289,-0.227 -0.069,-0.486 0.264,-0.451c0.615,0.174 1.125,0.596 1.605,1.222c0.478,0.627 0.703,0.769 1.596,0.769c0.433,0 1.081,-0.025 1.691,-0.121c0.328,-0.833 0.895,-1.6 1.588,-1.962c-3.996,-0.411 -5.903,-2.399 -5.903,-5.098c0,-1.162 0.495,-2.286 1.336,-3.233c-0.276,-0.94 -0.623,-2.857 0.106,-3.587c1.798,0 2.885,1.166 3.146,1.481c0.896,-0.307 1.88,-0.481 2.914,-0.481c1.036,0 2.024,0.174 2.922,0.483c0.258,-0.313 1.346,-1.483 3.148,-1.483c0.732,0.731 0.381,2.656 0.102,3.594c0.836,0.945 1.328,2.066 1.328,3.226c0,2.697 -1.904,4.684 -5.894,5.097c1.098,0.573 1.899,2.183 1.899,3.396v2.734c0,0.104 -0.023,0.179 -0.035,0.268c4.676,-1.639 8.035,-6.079 8.035,-11.315c0,-6.627 -5.373,-12 -12,-12z"
+									></path></g
+								></g
+							>
+						</svg>
 					</button>
 				{:else}
 					<details class="dropdown">
@@ -274,30 +333,48 @@
 						</div>
 					</div>
 				</div>
-				<div class="collapse">
-					<input type="checkbox" />
-					<div class="collapse-title flex items-center justify-center font-large">
-						Show/hide reply box
-					</div>
-					<div class="collapse-content flex flex-1 flex-col">
-						<div class="flex justify-center flex-1">
-							<div class="flex w-4/6 flex-col gap-1">
-								<div class="flex flex-1 w-full">
-									<CartaEditor bind:value={values[index]} mode="tabs" theme="github" {carta} />
-								</div>
-								<button
-									class="btn btn-active hover:btn-secondary"
-									type="submit"
-									on:click|preventDefault={() => sendComment(question.id, index)}
-								>
-									Comment!
-								</button>
-							</div>
+
+				<div class="mt-6 flex items-center justify-center font-large">
+					<button
+						class="btn btn-primary"
+						on:click|preventDefault={() => {
+							replyBoxId = question.id;
+							replyBoxIndex = index;
+						}}
+						>Propose an Answer
+					</button>
+				</div>
+			</div>
+		{/each}
+		{#if replyBoxId != null}
+			<div class="fixed bottom-0 w-full">
+				<div
+					class="flex justify-center flex-1 bg-slate-700 bg-opacity-60 rounded-tl-xl rounded-tr-xl"
+				>
+					<div class="flex w-4/6 flex-col gap-1 p-8">
+						<h2 class="text-lg mx-5 pl-16 font-bold bg-slate-700">Reply to {currentReply}</h2>
+						<div class="flex flex-1 w-full">
+							<CartaEditor bind:value={values[replyBoxIndex]} mode="tabs" theme="github" {carta} />
+						</div>
+						<div class="flex flex-row-reverse justify-around">
+							<button
+								class="btn btn-active hover:btn-secondary"
+								type="submit"
+								on:click|preventDefault={() => sendComment(replyBoxId, replyBoxIndex)}
+							>
+								Comment!
+							</button>
+							<button
+								class="btn btn-active hover:btn-neutral"
+								on:click|preventDefault={() => {
+									replyBoxId = null;
+								}}>cancel</button
+							>
 						</div>
 					</div>
 				</div>
 			</div>
-		{/each}
+		{/if}
 	{/if}
 {:else}
 	<div class="flex justify-center mb-5">
