0d06292ecef945919582155f16367a2264480963
Samuele Musiani <samu@teapot.ovh>
2024-06-06 18:53:43 +0200

    test fix

--- src/lib/teachings.ts
+++ src/lib/teachings.ts
@@ -2,81 +2,81 @@ import { getManifest } from '$lib/api';
 import { filterAsync } from './filter';
 
 export type Teaching = {
-	name: string;
-	url: string;
-	chat?: string;
-	website?: string;
-	professors?: string[];
-	degree?: string;
+  name: string;
+  url: string;
+  chat?: string;
+  website?: string;
+  professors?: string[];
+  degree?: string;
 };
 
 export type Year = {
-	year: number;
-	chat?: string;
+  year: number;
+  chat?: string;
 };
 
 export type DegreeTeaching = {
-	name: string;
-	year?: number;
-	mandatory: boolean;
+  name: string;
+  year?: number;
+  mandatory: boolean;
 };
 
 export type Degree = {
-	id: string;
-	name: string;
-	icon: string;
-	teachings?: DegreeTeaching[];
-	years?: Year[];
-	chat?: string;
+  id: string;
+  name: string;
+  icon: string;
+  teachings?: DegreeTeaching[];
+  years?: Year[];
+  chat?: string;
 };
 
 async function isTeachingActiveFromName(fetch: typeof window.fetch, teaching: string) {
-	try {
-		await getManifest(fetch, teaching);
-		return true;
-	} catch {
-		return false;
-	}
+  try {
+    await getManifest(fetch, teaching);
+    return true;
+  } catch {
+    return false;
+  }
 }
 
 export async function isTeachingActive(fetch: typeof window.fetch, teaching: Teaching) {
-	return isTeachingActiveFromName(fetch, teaching.url);
+  return isTeachingActiveFromName(fetch, teaching.url);
 }
 
 export function teachingsFilter(d: Degree, year?: number, mandatory?: boolean): string[] {
-	let res: string[] = [];
+  let res: string[] = [];
 
-	d.teachings?.forEach((x) => {
-		if (
-			(year == undefined || (x.year && x.year == year) || (!x.year && year == 0)) &&
-			(mandatory == undefined || x.mandatory == mandatory)
-		) {
-			res = res.concat(x.name);
-		}
-	});
+  d.teachings?.forEach((x) => {
+    if (
+      (year == undefined || (x.year && x.year == year) || (!x.year && year == 0)) &&
+      (mandatory == undefined || x.mandatory == mandatory)
+    ) {
+      res = res.concat(x.name);
+    }
+  });
 
-	return res;
+  return res;
 }
 
 export async function getActiveTeachings(
-	fetch: typeof window.fetch,
-	degree: Degree
+  fetch: typeof window.fetch,
+  degree: Degree
 ): Promise<string[]> {
-	const years = degree.years;
-	if (!years) return [];
-	const allTeachings = teachingsFilter(degree);
-	const activeTeachings = await filterAsync(allTeachings, (t) =>
-		isTeachingActiveFromName(fetch, t)
-	);
+  const years = degree.teachings;
+  if (!years) return [];
+  const allTeachings = teachingsFilter(degree);
+  const activeTeachings = await filterAsync(allTeachings, (t) =>
+    isTeachingActiveFromName(fetch, t)
+  );
 
-	return activeTeachings;
+  return activeTeachings;
 }
 
 // TEACHINGS
 import teachingsArray from '../config/teachings.json' assert { type: 'json' };
 
 function teachingToIndexedTeaching(t: Teaching): [string, Teaching] {
-	return [t.url, t];
+  return [t.url, t];
 }
 
 const TEACHINGS = new Map<string, Teaching>(teachingsArray.map(teachingToIndexedTeaching));
