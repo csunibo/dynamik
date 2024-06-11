7b10e81dafbdcbdcf48dec7bbb5f8b151551556e
Samuele Musiani <samu@teapot.ovh>
2024-06-11 15:09:19 +0200

    fix download button

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -1,6 +1,6 @@
 const ORG = 'csunibo';
 export const RISORSE_BASE_URL =
-	process.env.VITE_RISORSE_BASE_URL || 'https://risorse.students.cs.unibo.it';
+  process.env.VITE_RISORSE_BASE_URL || 'https://risorse.students.cs.unibo.it';
 export const GH_PAGES_BASE_URL = `https://${ORG}.github.io`;
 const GH_BASE_URL = `https://github.com/${ORG}`;
 const GH_DEV_BASE_URL = `https://github.dev/${ORG}`;
@@ -8,13 +8,13 @@ export const STATIK_URL = (path: string) => `${GH_PAGES_BASE_URL}/${path}/statik
 export const ASSET_URL = (path: string) => `${GH_PAGES_BASE_URL}/${path}`;
 export const FUZZY_URL = (path: string) => `${GH_PAGES_BASE_URL}/${path}/fuzzy.json`;
 export const EDIT_URLS = (path: string) => {
-	const [, repo, ...rest] = path.split('/');
-	const filePath = rest.join('/');
-	return {
-		github: `${GH_BASE_URL}/${repo}/blob/main/${filePath}`,
-		github_repo: `${GH_BASE_URL}/${repo}`,
-		github_edit: `${GH_BASE_URL}/${repo}/edit/main/${filePath}`,
-		github_dev: `${GH_DEV_BASE_URL}/${repo}/blob/main/${filePath}`
-	};
+  const [, repo, ...rest] = path.split('/');
+  const filePath = rest.join('/');
+  return {
+    github: `${GH_BASE_URL}/${repo}/blob/main/${filePath}`,
+    github_repo: `${GH_BASE_URL}/${repo}`,
+    github_edit: `${GH_BASE_URL}/${repo}/edit/main/${filePath}`,
+    github_dev: `${GH_DEV_BASE_URL}/${repo}/blob/main/${filePath}`
+  };
 };
 export const MAX_YEARS_FOR_DEGREE = 3;
