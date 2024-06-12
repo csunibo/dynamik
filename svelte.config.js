bf75bb2ca8fe92bf470b33da040ad15f98444f1b
Samuele Musiani <samu@teapot.ovh>
2024-06-12 09:03:10 +0200

    removed adapter for pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,9 @@
-import staticAdapter from '@sveltejs/adapter-static';
 import vercelAdapter from '@sveltejs/adapter-vercel';
 import nodeAdapter from '@sveltejs/adapter-node';
 
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const BASE_PATH = process.env.BASE_PATH || '';
-const GITHUB_PAGES = process.env.GITHUB_ACTIONS === 'true';
 const CSUNIBO_DEPLOY = process.env.CSUNIBO_DEPLOY === 'true';
 
 /**
@@ -13,30 +11,28 @@ const CSUNIBO_DEPLOY = process.env.CSUNIBO_DEPLOY === 'true';
  * @returns {import('@sveltejs/kit').Adapter}
  */
 function chooseAdapter() {
-	if (GITHUB_PAGES) {
-		return staticAdapter({ fallback: '404.html' });
-	} else if (CSUNIBO_DEPLOY) {
-		return nodeAdapter();
-	} else {
-		return vercelAdapter();
-	}
+  if (CSUNIBO_DEPLOY) {
+    return nodeAdapter();
+  } else {
+    return vercelAdapter();
+  }
 }
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
 
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: chooseAdapter(),
-		paths: {
-			base: BASE_PATH == '' ? '' : `/${BASE_PATH}`
-		}
-	}
+  kit: {
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    adapter: chooseAdapter(),
+    paths: {
+      base: BASE_PATH == '' ? '' : `/${BASE_PATH}`
+    }
+  }
 };
 
 export default config;
