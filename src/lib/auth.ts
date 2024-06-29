97dd66245d30f0677be56f0a60a4c1ebe161b3c5
Samuele Musiani <samu@teapot.ovh>
2024-06-29 22:16:27 +0200

    http to https

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,5 @@
 export const AUTH_BASE_URL =
-  import.meta.env.VITE_AUTH_BASE_URL || 'http://cslogin.students.cs.unibo.it';
+  import.meta.env.VITE_AUTH_BASE_URL || 'https://cslogin.students.cs.unibo.it';
 
 export const LOGIN_URL = (current_uri: string) =>
   `${AUTH_BASE_URL}/login?redirect_uri=${current_uri}`;
--- src/lib/const.ts
+++ src/lib/const.ts
@@ -1,6 +1,6 @@
 const ORG = 'csunibo';
 export const RISORSE_BASE_URL =
-	import.meta.env.VITE_RISORSE_BASE_URL || 'https://risorse.students.cs.unibo.it';
+  import.meta.env.VITE_RISORSE_BASE_URL || 'https://risorse.students.cs.unibo.it';
 export const GH_PAGES_BASE_URL = `https://${ORG}.github.io`;
 const GH_BASE_URL = `https://github.com/${ORG}`;
 const GH_DEV_BASE_URL = `https://github.dev/${ORG}`;
@@ -8,20 +8,20 @@ export const STATIK_URL = (path: string) => `${GH_PAGES_BASE_URL}/${path}/statik
 export const ASSET_URL = (path: string) => `${GH_PAGES_BASE_URL}/${path}`;
 export const FUZZY_URL = (path: string) => `${GH_PAGES_BASE_URL}/${path}/fuzzy.json`;
 export const EDIT_URLS = (path: string) => {
-	const [, repo, ...rest] = path.split('/');
-	const filePath = rest.join('/');
-	return {
-		github: `${GH_BASE_URL}/${repo}/blob/main/${filePath}`,
-		github_repo: `${GH_BASE_URL}/${repo}`,
-		github_edit: `${GH_BASE_URL}/${repo}/edit/main/${filePath}`,
-		github_dev: `${GH_DEV_BASE_URL}/${repo}/blob/main/${filePath}`
-	};
+  const [, repo, ...rest] = path.split('/');
+  const filePath = rest.join('/');
+  return {
+    github: `${GH_BASE_URL}/${repo}/blob/main/${filePath}`,
+    github_repo: `${GH_BASE_URL}/${repo}`,
+    github_edit: `${GH_BASE_URL}/${repo}/edit/main/${filePath}`,
+    github_dev: `${GH_DEV_BASE_URL}/${repo}/blob/main/${filePath}`
+  };
 };
 export const MAX_YEARS_FOR_DEGREE = 3;
 
 
-export const POLLEG_BASE_URL = 
-	import.meta.env.VITE_RISORSE_BASE_URL || 'http://polleg.students.cs.unibo.it';
+export const POLLEG_BASE_URL =
+  import.meta.env.VITE_RISORSE_BASE_URL || 'https://polleg.students.cs.unibo.it';
 export const DOCUMENT_URL = (id: string) => `${POLLEG_BASE_URL}/documents/${id}`;
 export const QUESTION_URL = (id: number) => `${POLLEG_BASE_URL}/questions/${id}`;
 export const ANSWER_URL = (id: number) => `${POLLEG_BASE_URL}/answers/${id}`;
