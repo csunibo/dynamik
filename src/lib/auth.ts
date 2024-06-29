512c3bb93cbf8fc6cd838304ff975adc34f15b43
Luca <luca@teapot.ovh>
2024-06-29 19:27:40 +0200

    fix login url?

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,4 +4,4 @@ export const AUTH_BASE_URL =
 export const LOGIN_URL = (current_uri: string) =>
   `${AUTH_BASE_URL}/login?redirect_uri=${current_uri}`;
 export const WHOAMI_URL = `${AUTH_BASE_URL}/whoami`;
-export const LOGOUT_URI = `${AUTH_BASE_URL}/logout`;
+export const LOGOUT_URI = (current_uri: string) => `${AUTH_BASE_URL}/logout?redirect_uri=${current_uri}`;
